3b88d121b0dd92ef197e53605fbac9127917d7a7
Nizam CS <nizam.c@cycloides.co.in>
2021-04-02 01:51:40 +0530

    documentation

--- lib/stateful_loader.dart
+++ lib/stateful_loader.dart
@@ -3,12 +3,17 @@ library stateful_loader;
 import 'package:flutter/material.dart';
 
 /// use as an extension mixin for stateful widget's State to directly call showLoading(context) and hideLoading()
-mixin LoadingWidget { 
+mixin LoadingWidget {
   BuildContext _dialogContext;
   bool _loaderRendering = false;
+  /// Can override to provide any widget as a loader
   Widget get loaderWidget => null;
+  /// Decides whether to dismiss the loader on taping the barrier. Defaults to false.
   bool get barrierDismissible => false;
+  /// Replace the boring black shade for the barrier
+  Color get barrierColor => null;
 
+  /// Call this to show a full screen loader
   showLoading(BuildContext context) async {
     if (_dialogContext != null) {
       Navigator.pop(_dialogContext);
@@ -18,6 +23,7 @@ mixin LoadingWidget {
     _dialogContext = await showDialog(
         context: context,
         barrierDismissible: barrierDismissible,
+        barrierColor: barrierColor,
         builder: (BuildContext context) {
           _loaderRendering = false;
           _dialogContext = context;
@@ -28,6 +34,7 @@ mixin LoadingWidget {
     _loaderRendering = false;
   }
 
+  /// Hides a previously shown loader through this mixin. Do nothing otherwise.
   hideLoading() {
     if (_loaderRendering) {
       Future.delayed(Duration(milliseconds: 500), () {
